dec2960be84f445002d715d2e78020392dd0cc23
Martin Gartner <martin.gartner@bitego.com>
2014-11-24 20:06:56 +0100

    Fixed another small bug in how cron security keys are checked
    Signed-off-by: Martin Gartner <martin.gartner@bitego.com>

--- assets/components/goodnews/cron.worker.php
+++ assets/components/goodnews/cron.worker.php
@@ -50,7 +50,7 @@ $modx->getService('error', 'error.modError', '', '');
 
 // If set - worker script may only be continued if the correct security key is provided by cron (@param sid)
 $securityKey = $modx->getOption('goodnews.cron_security_key', null, '');
-if ($_GET['sid'] !== $securityKey) {
+if ($_GET['sid'] != $securityKey) {
     exit('[GoodNews] cron.worker.php - Missing or wrong authentification! Sorry Dude!');
 }
 
