821ee4ae7d07a7ebaa1aa676df8d48780f338566
Martin Gartner <martin.gartner@bitego.com>
2015-03-24 15:29:50 +0100

    Added French translation (thank's Julien Studer!)
    Signed-off-by: Martin Gartner <martin.gartner@bitego.com>

---
+++ core/components/goodnews/lexicon/fr/frontend.inc.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * GoodNews
+ *
+ * Copyright 2012 by bitego <office@bitego.com>
+ *
+ * GoodNews is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * GoodNews is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this software; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+/**
+ * GoodNews frontend
+ *
+ * @package goodnews
+ * @subpackage lexicon
+ * @language fr
+ */
+
+$_lang['goodnews.user_err_save']                  = 'Une erreur s\'est produite lors de la sauvegarde de l\'utilisateur.';
+$_lang['goodnews.user_err_nf_email']              = 'Aucun utilisateur trouvé avec cette adresse e-mail.';
+$_lang['goodnews.profile_err_nf']                 = 'Profil introuvable!';
+$_lang['goodnews.profile_err_save']               = 'Une erreur inconnue s\'est produite lors de la modification de l\'utilisateur.';
+$_lang['goodnews.profile_err_unsubscribe']        = 'Une erreur inconnue s\'est produite lors de la suppression de l\'inscription.';
+$_lang['goodnews.profile_updated']                = 'Profil mise à jour.';
+$_lang['goodnews.profile_unsubscription']         = 'L\'inscription a été annulée.';
+
+$_lang['goodnews.email_no_recipient']             = 'Aucun destinataire spécifié pour l\'email.';
+$_lang['goodnews.email_not_sent']                 = 'Une erreur inconnue s\'est produite lors de l\'envoi de l\'email.';
+
+$_lang['goodnews.validator_form_error']           = 'Une erreur de validation du formulaire s\'est produite. Merci de contrôler les valeurs saisies.';
+$_lang['goodnews.validator_field_required']       = 'Ce champ est requis.';
+$_lang['goodnews.validator_field_not_empty']      = 'Ce champ doit être vide.';
+$_lang['goodnews.validator_email_taken']          = 'Cette adresse email est déjà utilisée. Merci d\'en introduire une autre.';
+$_lang['goodnews.validator_email_invalid']        = 'Merci de saisir une adresse email valide.';
+$_lang['goodnews.validator_email_invalid_domain'] = 'Votre adresse email n\'a pas un nom de domaine valide.';
+$_lang['goodnews.validator_min_length']           = 'Ce champs doit contenir au moins [[+length]] caractères.';
+$_lang['goodnews.validator_max_length']           = 'Ce champs ne peut pas contenir plus de [[+length]] caractères.';
+
+$_lang['goodnews.spam_blocked']                   = 'Votre inscription a été bloquée par un filtre antispam: ';
+$_lang['goodnews.spam_marked']                    = ' - marqué comme spam.';
+
+$_lang['goodnews.activation_email_subject']       = 'Merci de votre intérêt pour notre Newsletter. Merci d\'activer votre inscription!';
+$_lang['goodnews.subscription_email_subject']     = 'Merci de votre intérêt pour notre Newsletter. Votre inscription s\'est déroulée avec succès !';
+$_lang['goodnews.requestlinks_email_subject']     = 'Les liens demandés pour mettre à jour ou annuler votre souscription.';
+
+$_lang['goodnews.requestlinks_success']           = 'Les liens demandés ont été envoyés à l\'adresse email indiquée.';
+
+$_lang['goodnews.email']                          = 'Adresse Email';
+$_lang['goodnews.fullname']                       = 'Nom complet';
+$_lang['goodnews.address']                        = 'Adresse';
+$_lang['goodnews.city']                           = 'Ville';
+$_lang['goodnews.zip']                            = 'NPA';
+$_lang['goodnews.state']                          = 'Canton';
+$_lang['goodnews.country']                        = 'Pays';
+$_lang['goodnews.email']                          = 'Email';
+$_lang['goodnews.mobilephone']                    = 'Téléphone mobile';
+$_lang['goodnews.phone']                          = 'Téléphone';
+$_lang['goodnews.fax']                            = 'Fax';
+$_lang['goodnews.website']                        = 'Site Web';
+$_lang['goodnews.update_profile']                 = 'Mettre à jour le profil';
---
+++ core/components/goodnews/lexicon/fr/resource.inc.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ * GoodNews
+ *
+ * Copyright 2012 by bitego <office@bitego.com>
+ *
+ * GoodNews is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * GoodNews is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this software; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+/**
+ * GoodNews resource
+ *
+ * @package goodnews
+ * @subpackage lexicon
+ * @language fr
+ */
+
+$_lang['goodnews.container']                               = 'GoodNews';
+$_lang['goodnews.container_create_here']                   = 'Conteneur GoodNews';
+$_lang['goodnews.container_new']                           = 'Nouveau Conteneur GoodNews';
+                                                           
+$_lang['goodnews.container_tab_container']                 = 'GoodNews';
+$_lang['goodnews.container_tab_settings_container']        = 'Paramètres Conteneur';
+$_lang['goodnews.container_tab_settings_mailings']         = 'Paramètres Mailing';
+                                                           
+$_lang['goodnews.container_properties']                    = 'Propriétés du Conteneur';
+                                                           
+$_lang['goodnews.container_templates_category']            = 'Catégorie des Modèles de Mailing';
+$_lang['goodnews.container_templates_category_desc']       = 'La catégorie depuis laquelle sélectionner les modèles de mailing.';
+$_lang['goodnews.container_mailing_template']              = 'Modèle de Mailing par défaut';
+$_lang['goodnews.container_mailing_template_desc']         = 'Modèle par défaut pour les mailings depuis ce conteneur.';
+$_lang['goodnews.container_unsubscribe_resource']          = 'ID de la Ressource pour la désinscription en 1 clic';
+$_lang['goodnews.container_unsubscribe_resource_desc']     = 'Déplacer la Ressource pour la désinscription en 1 clic depuis l\'arborescence MODx dans ce champ.';
+$_lang['goodnews.container_profile_resource']              = 'ID de la Ressource pour éditer le profil de mailing';
+$_lang['goodnews.container_profile_resource_desc']         = 'Déplacer la Ressource pour éditer le profil de mailing depuis l\'arborescence MODx dans ce champ.';
+
+$_lang['goodnews.container_manage']                        = 'Gestion du Conteneur GoodNews';
+$_lang['goodnews.mailing_create_new']                      = 'Créer un nouveau Mailing';
+$_lang['goodnews.container_unpublish']                     = 'Dépublier le Conteneur GoodNews';
+$_lang['goodnews.container_publish']                       = 'Publier le Conteneur GoodNews';
+$_lang['goodnews.container_undelete']                      = 'Ne pas supprimer le Conteneur GoodNews';
+$_lang['goodnews.container_delete']                        = 'Supprimer le Conteneur GoodNews';
+$_lang['goodnews.container_view']                          = 'Voir le Conteneur GoodNews';
+$_lang['goodnews.manage_mailings']                         = 'Gestion des Mailings';
+
+$_lang['goodnews.mailing']                                 = 'Mailing';
+$_lang['goodnews.mailing_new']                             = 'Nouveau Mailing';
+$_lang['goodnews.mail_subject']                            = 'Sujet du Mail';
+$_lang['goodnews.mail_subject_desc']                       = 'Le sujet du mail.';
+$_lang['goodnews.mail_summary']                            = 'Résumé du mail';
+$_lang['goodnews.mail_summary_desc']                       = 'Un bref résumé du contenu du mail.';
+$_lang['goodnews.mail_body']                               = 'Corps du mail';
+$_lang['goodnews.mail_publishing_information']             = 'Publiers les informations';
+$_lang['goodnews.mail_status']                             = 'Statut';
+$_lang['goodnews.mail_options']                            = 'Options du mail';
+$_lang['goodnews.mail_sending_scheduled']                  = 'Envoi programmé';
+$_lang['goodnews.mail_sending_scheduled_desc']             = 'Spécifier une date ultérieure à laquelle le mail sera envoyé automatiquement.';
+$_lang['goodnews.mail_send_to']                            = 'Envoyé à';
+$_lang['goodnews.mail_groups_categories']                  = 'Groupes et Catégories';
+$_lang['goodnews.mail_template']                           = 'Modèle du mail';
+$_lang['goodnews.mail_template_desc']                      = 'Le modèle pour cet e-mail.';
+$_lang['goodnews.mail_format']                             = 'Format du mail';
+$_lang['goodnews.mail_format_desc']                        = 'Le format du corps du mail.';
+$_lang['goodnews.mail_format_html']                        = 'HTML';
+$_lang['goodnews.mail_format_plaintxt']                    = 'Texte brut';
+
+$_lang['goodnews.mailing_resource_collection']             = 'Collection: ';
+$_lang['goodnews.mailing_resource_collection_desc']        = 'Sélectionner des documents Resources à insérer automatiquement dans le corps du mail (p.ex. une liste de nouvelles entrées).';
+$_lang['goodnews.mailing_rc_container']                    = 'Conteneur';
+$_lang['goodnews.mailing_rc_resource']                     = 'Ressource';
+$_lang['goodnews.mailing_rc_resources']                    = 'Ressources';
+$_lang['goodnews.mailing_rc_resources_none']               = 'Aucun document Ressource';
+$_lang['goodnews.mailing_rc_parent_filter']                = 'Filtrer par Conteneur';
+$_lang['goodnews.mailing_rc_resources_all']                = 'Tous les Documents Ressource';
+$_lang['goodnews.mailing_rc_search_filter']                = 'Filtre de recherche...';
+$_lang['goodnews.mailing_rc_filter_clear']                 = 'Réinitialiser';
+
+$_lang['setting_goodnews.default_container_template']      = 'Modèle Conteneur GoodNews par défaut';
+$_lang['setting_goodnews.default_container_template_desc'] = 'Le modèle par défaut à utiliser lors de la création d\'un nouveau conteneur GoodNews.';
+
+$_lang['setting_goodnews.default_mailing_template']        = 'Modèle de mailing/newsletter GoodNews par défaut';
+$_lang['setting_goodnews.default_mailing_template_desc']   = 'Le modèle par défaut à utiliser lors de la création d\'un nouveau mailing/newsletter, quand il n\'y en a pas de spécifié dans le Conteneur GoodNews lui-même.';
