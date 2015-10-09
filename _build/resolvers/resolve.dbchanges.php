21a29b879452d07080f965b9e6680bfb7e317133
gadgetto <rij-Ub-eiD-yab-Yak-Ek>
2015-10-09 14:29:07 +0200

    Fixed error messages in GoodNews installer while updating database schema
    Signed-off-by: gadgetto <rij-Ub-eiD-yab-Yak-Ek>

--- _build/resolvers/resolve.dbchanges.php
+++ _build/resolvers/resolve.dbchanges.php
@@ -25,6 +25,35 @@
  * @subpackage build
  */
 
+/**
+ * Checks if a field in a specified database table exist and creates it if not.
+ * (this prevents the annoying erro messages in MODx install log)
+ * 
+ * @param mixed &$modx A reference to the MODX object
+ * @param mixed &$manager A reference to the Manager object
+ * @param string $xpdoTableClass xPDO schema class name for the database table
+ * @param string $field Name of the field to create
+ * @param string $after Name of the field after which the new field should be placed (Optional)
+ * @return void
+ */
+if (!function_exists('checkAddField')) {
+    function checkAddField(&$modx, &$manager, $xpdoTableClass, $field, $after = '') {
+
+        $table = $modx->getTableName($xpdoTableClass);
+        $sql = "SHOW COLUMNS FROM {$table} LIKE '".$field."'";
+        $stmt = $modx->prepare($sql);
+        $stmt->execute();
+        $count = $stmt->rowCount();
+        $stmt->closeCursor();
+        
+        if ($count < 1) {
+            $options = array();
+            if (!empty($after)) $options['after'] = $after;
+            $manager->addField($xpdoTableClass, $field, $options);
+        }
+    }
+}
+
 if ($object->xpdo) {
     $modx = &$object->xpdo;
     
@@ -42,23 +71,23 @@ if ($object->xpdo) {
             $modx->setLogLevel(xPDO::LOG_LEVEL_ERROR);
 
             // 1.1.0-pl+
-            $manager->addField('GoodNewsSubscriberMeta', 'hard_bounces', array('after' => 'ip'));
-            $manager->addField('GoodNewsSubscriberMeta', 'soft_bounces', array('after' => 'ip'));
-            $manager->addField('GoodNewsMailingMeta', 'hard_bounces', array('after' => 'scheduled'));
-            $manager->addField('GoodNewsMailingMeta', 'soft_bounces', array('after' => 'scheduled'));
+            checkAddField($modx, $manager, 'GoodNewsSubscriberMeta', 'hard_bounces', 'ip');
+            checkAddField($modx, $manager, 'GoodNewsSubscriberMeta', 'soft_bounces', 'ip');
+            checkAddField($modx, $manager, 'GoodNewsMailingMeta', 'hard_bounces', 'scheduled');
+            checkAddField($modx, $manager, 'GoodNewsMailingMeta', 'soft_bounces', 'scheduled');
 
             // 1.2.0-pl+
-            $manager->addField('GoodNewsMailingMeta', 'recipients_error', array('after' => 'recipients_sent'));
+            checkAddField($modx, $manager, 'GoodNewsMailingMeta', 'recipients_error', 'recipients_sent');
             $manager->createObjectContainer('GoodNewsRecipient');
             $manager->createObjectContainer('GoodNewsSubscriberLog');
             
             //GoodNewsMailingMeta - recipients_list field deprecated since 1.2.0-pl+
 
             // 1.3.0-pl+
-            $manager->addField('GoodNewsMailingMeta', 'collections', array('after' => 'categories'));
+            checkAddField($modx, $manager, 'GoodNewsMailingMeta', 'collections', 'categories');
             
             // 1.3.9-pl+
-            $manager->addField('GoodNewsGroup', 'public', array('after' => 'description'));
+            checkAddField($modx, $manager, 'GoodNewsGroup', 'public', 'description');
             
             // Set bakck log-level to previous level
             $modx->setLogLevel($oldLogLevel);
