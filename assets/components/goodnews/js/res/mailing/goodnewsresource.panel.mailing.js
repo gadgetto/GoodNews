ed6019348a26b28246797dca226d3d6191490c4a
gadgetto <rij-Ub-eiD-yab-Yak-Ek>
2015-12-09 14:19:59 +0100

    Fixed a bug with tvs_below_content setting in Mailing editor
    Signed-off-by: gadgetto <rij-Ub-eiD-yab-Yak-Ek>

--- assets/components/goodnews/js/res/mailing/goodnewsresource.panel.mailing.js
+++ assets/components/goodnews/js/res/mailing/goodnewsresource.panel.mailing.js
@@ -124,9 +124,7 @@ Ext.extend(GoodNewsResource.panel.Mailing,MODx.panel.Resource,{
         });
 
         if (MODx.config.tvs_below_content == 1) {
-            var tvs = this.getTemplateVariablesPanel(config);
-            tvs.style = 'margin-top: 10px';
-            its.push(tvs);
+            its.push(this.getTemplateVariablesPanel(config));
         }
         return its;
     }
