cc1de5698d532997c8bc643fb9860a1a5bb5b36d
gadgetto <martin.gartner@bitego.com>
2018-07-18 19:29:23 +0200

    Push version number

--- core/components/goodnews/model/goodnews/goodnews.class.php
+++ core/components/goodnews/model/goodnews/goodnews.class.php
@@ -28,7 +28,7 @@ class GoodNews {
 
     const NAME     = 'GoodNews';
     const VERSION  = '1.5.0';
-    const RELEASE  = 'beta5';
+    const RELEASE  = 'beta6';
     
     const HELP_URL = 'http://www.bitego.com/extras/goodnews/';
     const DEV_NAME = 'bitego (Martin Gartner, Franz Gallei)';
