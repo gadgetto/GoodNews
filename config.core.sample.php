db151830dd38736b979c2bf2a0652b964067a167
Martin Gartner <martin.gartner@bitego.com>
2022-11-28 14:19:45 +0100

    Updated descriptions in config.core.sample.php

--- config.core.sample.php
+++ config.core.sample.php
@@ -3,14 +3,15 @@
  * This config.core.sample.php includes the original config.core.php file
  * from your MODX installation based on this folder structure:
  *
- * /modx/  (<- the MODX root)
+ * https://your.domain.dev/
+ * modx/  (<- the MODX root)
  * - - assets/
  * - - connectors/
  * - - core/
  * - - manager/
  * - - config.core.php  (<- the config.core.php from MODX)
  * - - index.php
- * /projects/  (<- your dev projects folder)
+ * projects/  (<- your dev projects folder)
  * - - goodnews/
  * - - - - config.core.php  (<- we are here)
  * - - project2/
@@ -18,6 +19,19 @@
  * - - project3/
  * - - - - config.core.php
  *
+ * PLEASE NOTE:
+ * 
+ * To use this project structure (subfolder MODX installation and development outside of MODX root)   
+ * you need to change the following "session_" system settings in all MODX installations:
+ * 
+ * MODX 2 install sample:
+ *   session_name = EXTRAS2x  (<- different session name in all installations)
+ *   session_cookie_path = /  (<- needs to be same in all installations)
+ * 
+ * MODX 3 install sample:
+ *   session_name = EXTRAS3x
+ *   session_cookie_path = /
+ * 
  * Be sure to modify this to your needs and rename this config.core.sample.php file
  * to config.core.php and to NOT include it in the git repo.
  */
