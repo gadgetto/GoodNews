4ffe14ea647205861eea7a30401ebb570d2f787a
Martin Gartner <martin.gartner@bitego.com>
2022-12-09 17:47:11 +0100

    create.dbtables.php script updated for MODX 3

--- _build/create.dbtables.php
+++ _build/create.dbtables.php
@@ -18,16 +18,16 @@
  * Place, Suite 330, Boston, MA 02111-1307 USA
  */
 
+use MODX\Revolution\modX;
+use MODX\Revolution\Error\modError;
+
 /**
  * Create database tables script
- * (supports MODX version 2.3.0 up to 2.8.x)
+ * (supports MODX version 3.0.0 up to *)
  *
  * @package goodnews
  * @subpackage build
  */
-
-require_once dirname(__DIR__, 1) . '/config.core.php';
-require_once MODX_CORE_PATH . 'model/modx/modx.class.php';
 
 $mtime = microtime();
 $mtime = explode(' ', $mtime);
@@ -35,31 +35,84 @@ $mtime = $mtime[1] + $mtime[0];
 $tstart = $mtime;
 set_time_limit(0);
 
+/* Define package name and namespace */
+define('PKG_NAME', 'GoodNews');
+define('PKG_NAMESPACE', strtolower(PKG_NAME));
+
+$className = PKG_NAME . '\\' . PKG_NAME;
+
+$root = dirname(__DIR__, 1) . '/';
+$sources = array(
+    'root'             => $root,
+    'source_src'       => $root . 'core/components/' . PKG_NAMESPACE . '/src/',
+);
+unset($root);
+
+require_once $sources['root'] . 'config.core.php';
+require_once MODX_CORE_PATH . 'vendor/autoload.php';
+
+/* Load MODX */
 $modx = new modX();
 $modx->initialize('mgr');
-$modelPath = $modx->getOption('goodnews.core_path', null, $modx->getOption('core_path') . 'components/goodnews/') . 'model/';
+if (!$modx->services->has('error')) {
+    $modx->services->add('error', function($c) use ($modx) {
+        return new modError($modx);
+    });
+}
+$modx->error = $modx->services->get('error');
+$modx->setLogLevel(modX::LOG_LEVEL_INFO);
+$modx->setLogTarget('ECHO');
+echo '<pre>';
+flush();
 
-$modx->addPackage('goodnews', $modelPath);
+$modx->log(modX::LOG_LEVEL_INFO, 'Creating database tables...');
+
+/* Add package */
+if ($modx->addPackage(PKG_NAME . '\Model', $sources['source_src'], null, PKG_NAME . '\\')) {
+    $modx->log(modX::LOG_LEVEL_ERROR, PKG_NAME . ' package added.');
+} else {
+    $modx->log(modX::LOG_LEVEL_ERROR, PKG_NAME . ' package could not be added.');
+    $modx->log(modX::LOG_LEVEL_INFO, 'Building development environment failed!');
+    flush();
+    exit();
+}
+flush();
+
+/* Add package service */
+if (!$modx->services->has(PKG_NAMESPACE)) {
+    $modx->services->add(PKG_NAMESPACE, function($c) use ($modx, $className) {
+        return new $className($modx);
+    });
+}
+$service = $modx->services->get(PKG_NAMESPACE);
+if ($service instanceof $className) {
+    $modx->log(modX::LOG_LEVEL_ERROR, PKG_NAME . ' service loaded.');
+} else {
+    $modx->log(modX::LOG_LEVEL_ERROR, PKG_NAME . ' service could not be loaded.');
+    $modx->log(modX::LOG_LEVEL_INFO, 'Building development environment failed!');
+    flush();
+    exit();
+}
+flush();
+
+$tables = [
+    /*
+    'GoodNews\Model\GoodNewsMailingMeta',
+    'GoodNews\Model\GoodNewsRecipient',
+    'GoodNews\Model\GoodNewsSubscriberMeta',
+    'GoodNews\Model\GoodNewsSubscriberLog',
+    'GoodNews\Model\GoodNewsGroup',
+    'GoodNews\Model\GoodNewsGroupMember',
+    'GoodNews\Model\GoodNewsCategory',
+    'GoodNews\Model\GoodNewsCategoryMember',
+    'GoodNews\Model\GoodNewsProcess',
+    */
+];
 
 $manager = $modx->getManager();
-$modx->setLogLevel(modX::LOG_LEVEL_INFO);
-$modx->setLogTarget('HTML');
-
-$objects = array(
-    //'GoodNewsMailingMeta',
-    //'GoodNewsRecipient',
-    //'GoodNewsSubscriberMeta',
-    //'GoodNewsSubscriberLog',
-    //'GoodNewsGroup',
-    //'GoodNewsGroupMember',
-    //'GoodNewsCategory',
-    //'GoodNewsCategoryMember',
-    //'GoodNewsProcess',
-);
-
 $count = 0;
-foreach ($objects as $obj) {
-    $manager->createObjectContainer($obj);
+foreach ($tables as $table) {
+    $manager->createObjectContainer($table);
     $count++;
 }
 
@@ -70,4 +123,7 @@ $tend = $mtime;
 $totalTime = ($tend - $tstart);
 $totalTime = sprintf("%2.4f s", $totalTime);
 
-$modx->log(modX::LOG_LEVEL_INFO, "\n<br>{$count} Database tables created.<br>\nExecution time: {$totalTime}\n");
+$modx->log(modX::LOG_LEVEL_INFO, "{$count} Database tables created.");
+$modx->log(modX::LOG_LEVEL_INFO, "Execution time: {$totalTime}");
+echo '</pre>';
+flush();
