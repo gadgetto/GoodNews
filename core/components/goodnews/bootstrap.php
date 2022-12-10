577193cef2a61e507574168925bc6d08e5dcdea1
Martin Gartner <martin.gartner@bitego.com>
2022-12-10 11:31:37 +0100

    Updated bootstrrap.php to comply with PSR1,2,12

--- core/components/goodnews/bootstrap.php
+++ core/components/goodnews/bootstrap.php
@@ -1,12 +1,13 @@
 <?php
+
 /**
  * Bootstrap file for MODX 3.x
- * 
+ *
  * @var \MODX\Revolution\modX $modx
  * @var array $namespace
  */
 
 $modx->addPackage('GoodNews\Model', $namespace['path'] . 'src/', null, 'GoodNews\\');
-$modx->services->add('goodnews', function($c) use ($modx) {
+$modx->services->add('goodnews', function ($c) use ($modx) {
     return new GoodNews\GoodNews($modx);
 });
