3acfcf55e901d6b96ca3caf9dfdb8d2a950d1c3c
Martin Gartner <martin.gartner@bitego.com>
2022-12-11 23:20:31 +0100

    Rewrite of send test mailing processor

---
+++ core/components/goodnews/src/Processors/Send/SendTest.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * This file is part of the GoodNews package.
+ *
+ * @copyright bitego (Martin Gartner)
+ * @license GNU General Public License v2.0 (and later)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace GoodNews\Processors\Send;
+
+use MODX\Revolution\modSystemSetting;
+use MODX\Revolution\Processors\Processor;
+use GoodNews\GoodNewsMailing;
+
+/**
+ * Send test newsletters processor.
+ *
+ * @var \MODX\Revolution\modX $modx
+ * @package goodnews
+ * @subpackage processors
+ */
+class SendTest extends Processor
+{
+    /** @var GoodNewsMailing $goodnewsmailing */
+    public $goodnewsmailing = null;
+    
+    /** @var int $mailingid The resource id of the newsletter */
+    public $mailingid = 0;
+    
+    public function initialize()
+    {
+        $this->goodnewsmailing = new GoodNewsMailing($this->modx);
+        $this->mailingid = $this->getProperty('mailingid');
+        return parent::initialize();
+    }
+    
+    public function process()
+    {
+        if (!$this->goodnewsmailing) {
+            return $this->failure('GoodNewsMailing class could not be instantiated.');
+        }
+        // Send the test mails
+        $this->goodnewsmailing->processTestMailing($this->mailingid);
+        
+        return $this->success();
+    }
+}
