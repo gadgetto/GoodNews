785b99fd8ffa6403ec429ab5d22f925711726b14
Martin Gartner <martin.gartner@bitego.com>
2022-12-12 17:42:33 +0100

    Updated categories filter get list processor

---
+++ core/components/goodnews/src/Processors/Category/FilterGetList.php
@@ -0,0 +1,61 @@
+<?php
+
+/**
+ * This file is part of the GoodNews package.
+ *
+ * @copyright bitego (Martin Gartner)
+ * @license GNU General Public License v2.0 (and later)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace GoodNews\Processors\Category;
+
+use GoodNews\Model\GoodNewsCategory;
+use MODX\Revolution\Processors\Model\GetListProcessor;
+use xPDO\Om\xPDOObject;
+use xPDO\Om\xPDOQuery;
+
+/**
+ * Category filter get list processor
+ *
+ * @package goodnews
+ * @subpackage processors
+ */
+
+class FilterGetList extends GetListProcessor
+{
+    public $classKey = GoodNewsCategory::class;
+    public $languageTopics = array('goodnews:default');
+    public $defaultSortField = 'name';
+    public $defaultSortDirection = 'ASC';
+    public $objectType = 'goodnews.category';
+
+    public function prepareQueryBeforeCount(xPDOQuery $c)
+    {
+        $c->select(array(
+            'GoodNewsCategory.id',
+            'GoodNewsCategory.name',
+        ));
+        return $c;
+    }
+
+    public function prepareRow(xPDOObject $object)
+    {
+        $resourceArray =  $object->toArray('', false, true, true);
+        return $resourceArray;
+    }
+
+    public function beforeIteration(array $list)
+    {
+        // additional option value "no category assigned"
+        if ($this->getProperty('addNoCategoryOption', false)) {
+            $list[] = array(
+                'id' => 'nocategory',
+                'name' => $this->modx->lexicon('goodnews.subscribers_no_category'),
+            );
+        }
+        return $list;
+    }
+}
