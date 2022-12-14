129deb121affea5798dd10e29cf4829fb2b28fd1
Martin Gartner <martin.gartner@bitego.com>
2022-12-14 18:20:03 +0100

    Updated subscribers update processor

--- core/components/goodnews/src/Processors/Subscribers/Update.php
+++ core/components/goodnews/src/Processors/Subscribers/Update.php
@@ -1,34 +1,30 @@
 <?php
-/**
- * GoodNews
- *
- * Copyright 2012 by bitego <office@bitego.com>
- *
- * GoodNews is free software; you can redistribute it and/or modify it under the
- * terms of the GNU General Public License as published by the Free Software
- * Foundation; either version 2 of the License, or (at your option) any later
- * version.
- *
- * GoodNews is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
- * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along with
- * this software; if not, write to the Free Software Foundation, Inc., 59 Temple
- * Place, Suite 330, Boston, MA 02111-1307 USA
- */
 
 /**
- * Subscribers update processor (GoodNews categories, groups, testdummy)
+ * This file is part of the GoodNews package.
+ *
+ * @copyright bitego (Martin Gartner)
+ * @license GNU General Public License v2.0 (and later)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Bitego\GoodNews\Processors\Subscribers;
+
+use Bitego\GoodNews\Model\GoodNewsSubscriberMeta;
+use Bitego\GoodNews\Model\GoodNewsCategoryMember;
+use Bitego\GoodNews\Model\GoodNewsGroupMember;
+
+/**
+ * Subscribers update processor (GoodNews categories, groups, testdummy).
  *
  * @package goodnews
  * @subpackage processors
  */
 
-class SubscribersUpdateProcessor extends modProcessor {    
-
-    public $languageTopics = array('user', 'goodnews:default');
-
+class Update extends Processor
+{
     /** @var int $userid The resource id of the user */
     public $userid = 0;
     
@@ -43,20 +39,21 @@ class SubscribersUpdateProcessor extends modProcessor {
      *
      * @return mixed
      */
-    public function initialize() {
-        $this->userid           = $this->getProperty('id');
+    public function initialize()
+    {
+        $this->userid = $this->getProperty('id');
         $this->groupscategories = $this->getProperty('groupscategories');
-        $this->testdummy        = (bool)$this->getProperty('testdummy');
+        $this->testdummy = (bool)$this->getProperty('testdummy');
         return parent::initialize();
     }
-
-	/**
-	 * {@inheritDoc}
-     * 
+    
+    /**
+     * {@inheritDoc}
+     *
      * @return mixed
-	 */    
-	public function process() {
-
+     */
+    public function process()
+    {
         // Extract group and category IDs
         // (e.g. n_gongrp_5,n_goncat_6_5,n_goncat_5_5,n_gongrp_6,n_gongrp_7 )
         // $nodeparts[0] = 'n'
@@ -78,23 +75,23 @@ class SubscribersUpdateProcessor extends modProcessor {
         }
         
         // Remove all prior categories of this user
-        $result = $this->modx->removeCollection('GoodNewsCategoryMember', array('member_id' => $this->userid));
+        $result = $this->modx->removeCollection(GoodNewsCategoryMember::class, array('member_id' => $this->userid));
         if ($result == false && $result != 0) {
-            // todo: return specific error message
+            // @todo: return specific error message
             return $this->failure($this->modx->lexicon('user_err_save'));
         }
         
         // Remove all prior groups of this user
-        $result = $this->modx->removeCollection('GoodNewsGroupMember', array('member_id' => $this->userid));
+        $result = $this->modx->removeCollection(GoodNewsGroupMember::class, array('member_id' => $this->userid));
         if ($result == false && $result != 0) {
-            // todo: return specific error message
+            // @todo: return specific error message
             return $this->failure($this->modx->lexicon('user_err_save'));
         }
-
+        
         // Add new groups for this user
         $grpupdate = true;
         foreach ($groups as $group) {
-            $gongroupmember = $this->modx->newObject('GoodNewsGroupMember');
+            $gongroupmember = $this->modx->newObject(GoodNewsGroupMember::class);
             $gongroupmember->set('goodnewsgroup_id', $group);
             $gongroupmember->set('member_id', $this->userid);
             if (!$gongroupmember->save()) {
@@ -102,14 +99,14 @@ class SubscribersUpdateProcessor extends modProcessor {
             }
         }
         if (!$grpupdate) {
-            // todo: return specific error message
+            // @todo: return specific error message
             return $this->failure($this->modx->lexicon('user_err_save'));
         }
         
         // Add new categories for this user
         $catupdate = true;
         foreach ($categories as $category) {
-            $goncategorymember = $this->modx->newObject('GoodNewsCategoryMember');
+            $goncategorymember = $this->modx->newObject(GoodNewsCategoryMember::class);
             $goncategorymember->set('goodnewscategory_id', $category);
             $goncategorymember->set('member_id', $this->userid);
             if (!$goncategorymember->save()) {
@@ -117,27 +114,37 @@ class SubscribersUpdateProcessor extends modProcessor {
             }
         }
         if (!$catupdate) {
-            // todo: return specific error message
+            // @todo: return specific error message
             return $this->failure($this->modx->lexicon('user_err_save'));
         }
         
         // Write subscriber meta data
-        $meta = $this->modx->getObject('GoodNewsSubscriberMeta', array('subscriber_id' => $this->userid));
+        $meta = $this->modx->getObject(GoodNewsSubscriberMeta::class, array('subscriber_id' => $this->userid));
         if (!is_object($meta)) {
-            $meta = $this->modx->newObject('GoodNewsSubscriberMeta');
+            $meta = $this->modx->newObject(GoodNewsSubscriberMeta::class);
             $meta->set('subscriber_id', $this->userid);
             // Set member subscription date (this is not the creation date of the MODx user!)
             $meta->set('subscribedon', time());
-            $meta->set('sid', md5(time().$this->userid));
-            $meta->set('ip', 'manually');  // Set IP field to string 'manually' for later reference
+            $meta->set('sid', md5(time() . $this->userid));
+            // Set IP field to string 'manually' for later reference
+            $meta->set('ip', 'manually');
         }
         $meta->set('testdummy', $this->testdummy);
         
         if (!$meta->save()) {
-            // todo: return specific error message
+            // @todo: return specific error message
             return $this->failure($this->modx->lexicon('user_err_save'));
         }
         return $this->success();
-	}
+    }
+    
+    /**
+     * {@inheritDoc}
+     *
+     * @return array
+     */
+    public function getLanguageTopics()
+    {
+        return ['user', 'goodnews:default'];
+    }
 }
-return 'SubscribersUpdateProcessor';
