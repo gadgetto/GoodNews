5c2595e6e165b9aa46991a0f831c2f6ca8e6d07d
Martin Gartner <martin.gartner@bitego.com>
2022-12-14 18:30:06 +0100

    Updated subscribers assign multi processor

--- core/components/goodnews/src/Processors/Subscribers/AssignMulti.php
+++ core/components/goodnews/src/Processors/Subscribers/AssignMulti.php
@@ -1,34 +1,30 @@
 <?php
-/**
- * GoodNews
- *
- * Copyright 2012 by bitego <office@bitego.com>
- *
- * GoodNews is free software; you can redistribute it and/or modify it under the
- * terms of the GNU General Public License as published by the Free Software
- * Foundation; either version 2 of the License, or (at your option) any later
- * version.
- *
- * GoodNews is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
- * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along with
- * this software; if not, write to the Free Software Foundation, Inc., 59 Temple
- * Place, Suite 330, Boston, MA 02111-1307 USA
- */
 
 /**
- * GoodNews processor to assign groups, categories and testdummy flag to a batch of users
+ * This file is part of the GoodNews package.
+ *
+ * @copyright bitego (Martin Gartner)
+ * @license GNU General Public License v2.0 (and later)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Bitego\GoodNews\Processors\Subscribers;
+
+use Bitego\GoodNews\Model\GoodNewsSubscriberMeta;
+use Bitego\GoodNews\Model\GoodNewsCategoryMember;
+use Bitego\GoodNews\Model\GoodNewsGroupMember;
+
+/**
+ * GoodNews processor to assign groups, categories and testdummy flag to a batch of users.
  *
  * @package goodnews
  * @subpackage processors
  */
 
-class SubscribersAssignMultiProcessor extends modProcessor {
-
-    public $languageTopics = array('user', 'goodnews:default');
-
+class AssignMulti extends Processor
+{
     /** @var mixed $userIds Array/Comma separated list of user ids */
     public $userIds = 0;
     
@@ -43,26 +39,31 @@ class SubscribersAssignMultiProcessor extends modProcessor {
      *
      * @return mixed
      */
-    public function initialize() {
-        $this->userIds          = $this->getProperty('userIds', '');
+    public function initialize()
+    {
+        $this->userIds = $this->getProperty('userIds', '');
         $this->groupscategories = $this->getProperty('groupscategories', '');
-        $this->testdummy        = (bool)$this->getProperty('testdummy', false);
-        $this->replaceGrpsCats  = (bool)$this->getProperty('replaceGrpsCats', false);
+        $this->testdummy = (bool)$this->getProperty('testdummy', false);
+        $this->replaceGrpsCats = (bool)$this->getProperty('replaceGrpsCats', false);
         return parent::initialize();
     }
 
-	/**
-	 * {@inheritDoc}
-     * 
+    /**
+     * {@inheritDoc}
+     *
      * @return mixed
-	 */    
-    public function process() {
-
+     */
+    public function process()
+    {
         if (empty($this->userIds)) {
             return $this->failure($this->modx->lexicon('goodnews.subscriber_err_ns_multi'));
         }
-        $this->userIds = is_array($this->userIds) ? $this->userIds : explode(',', $this->userIds);
-        $nodes = is_array($this->groupscategories) ? $this->groupscategories : explode(',', $this->groupscategories);
+        $this->userIds = is_array($this->userIds)
+            ? $this->userIds
+            : explode(',', $this->userIds);
+        $nodes = is_array($this->groupscategories)
+            ? $this->groupscategories
+            : explode(',', $this->groupscategories);
 
         // Extract group and category IDs
         // (e.g. n_gongrp_5,n_goncat_6_5,n_goncat_5_5,n_gongrp_6,n_gongrp_7 )
@@ -86,57 +87,58 @@ class SubscribersAssignMultiProcessor extends modProcessor {
         }
 
         foreach ($this->userIds as $id) {
-            if (empty($id)) { continue; }
+            if (empty($id)) {
+                continue;
+            }
             
-            $groupsToAdd          = array();
-            $categoriesToAdd      = array();
-            $prevGroupsMember     = array();
-            $prevCategoriesMember = array();
+            $groupsToAdd = [];
+            $categoriesToAdd = [];
+            $prevGroupsMember = [];
+            $prevCategoriesMember = [];
             
             // Ignore previous groups and categories (REPLACE)
             if ($this->replaceGrpsCats) {
-                
                 $groupsToAdd = $groups;
                 $categoriesToAdd = $categories;
-                
             // Merge previous groups and categories (ADD)
             } else {
-
-                $grpObj = $this->modx->getIterator('GoodNewsGroupMember', array('member_id' => $id));
+                $grpObj = $this->modx->getIterator(GoodNewsGroupMember::class, array('member_id' => $id));
                 foreach ($grpObj as $idx => $grp) {
                     $prevGroupsMember[] .= $grp->get('goodnewsgroup_id');
                 }
                 
-                $catObj = $this->modx->getIterator('GoodNewsCategoryMember', array('member_id' => $id));
+                $catObj = $this->modx->getIterator(GoodNewsCategoryMember::class, array('member_id' => $id));
                 foreach ($catObj as $idx => $cat) {
                     $prevCategoriesMember[] .= $cat->get('goodnewscategory_id');
                 }
                 
                 $groupsToAdd = array_merge($prevGroupsMember, $groups);
-                $groupsToAdd = array_keys(array_flip($groupsToAdd)); // remove duplicates
+                // remove duplicates
+                $groupsToAdd = array_keys(array_flip($groupsToAdd));
                 
                 $categoriesToAdd = array_merge($prevCategoriesMember, $categories);
-                $categoriesToAdd = array_keys(array_flip($categoriesToAdd)); // remove duplicates
+                // remove duplicates
+                $categoriesToAdd = array_keys(array_flip($categoriesToAdd));
             }
 
             // Remove all prior categories of this user
-            $result = $this->modx->removeCollection('GoodNewsCategoryMember', array('member_id' => $id));
+            $result = $this->modx->removeCollection(GoodNewsCategoryMember::class, array('member_id' => $id));
             if ($result == false && $result != 0) {
-                // todo: return specific error message
+                // @todo: return specific error message
                 return $this->failure($this->modx->lexicon('user_err_save'));
             }
             
             // Remove all prior groups of this user
-            $result = $this->modx->removeCollection('GoodNewsGroupMember', array('member_id' => $id));
+            $result = $this->modx->removeCollection(GoodNewsGroupMember::class, array('member_id' => $id));
             if ($result == false && $result != 0) {
-                // todo: return specific error message
+                // @todo: return specific error message
                 return $this->failure($this->modx->lexicon('user_err_save'));
             }
     
             // Add new groups for this user
             $grpupdate = true;
             foreach ($groupsToAdd as $group) {
-                $gongroupmember = $this->modx->newObject('GoodNewsGroupMember');
+                $gongroupmember = $this->modx->newObject(GoodNewsGroupMember::class);
                 $gongroupmember->set('goodnewsgroup_id', $group);
                 $gongroupmember->set('member_id', $id);
                 if (!$gongroupmember->save()) {
@@ -151,7 +153,7 @@ class SubscribersAssignMultiProcessor extends modProcessor {
             // Add new categories for this user
             $catupdate = true;
             foreach ($categoriesToAdd as $category) {
-                $goncategorymember = $this->modx->newObject('GoodNewsCategoryMember');
+                $goncategorymember = $this->modx->newObject(GoodNewsCategoryMember::class);
                 $goncategorymember->set('goodnewscategory_id', $category);
                 $goncategorymember->set('member_id', $id);
                 if (!$goncategorymember->save()) {
@@ -164,14 +166,15 @@ class SubscribersAssignMultiProcessor extends modProcessor {
             }
             
             // Write subscriber meta data
-            $meta = $this->modx->getObject('GoodNewsSubscriberMeta', array('subscriber_id' => $id));
+            $meta = $this->modx->getObject(GoodNewsSubscriberMeta::class, array('subscriber_id' => $id));
             if (!is_object($meta)) {
-                $meta = $this->modx->newObject('GoodNewsSubscriberMeta');
+                $meta = $this->modx->newObject(GoodNewsSubscriberMeta::class);
                 $meta->set('subscriber_id', $id);
-                // Set member subscription date (this is not the creation date of the MODx user!)
+                // Set member subscription date (this is not the creation date of the MODX user!)
                 $meta->set('subscribedon', time());
-                $meta->set('sid', md5(time().$id));
-                $meta->set('ip', 'manually');  // Set IP field to string 'manually' for later reference
+                $meta->set('sid', md5(time() . $id));
+                // Set IP field to string 'manually' for later reference
+                $meta->set('ip', 'manually');
             }
             $meta->set('testdummy', $this->testdummy);
             
@@ -182,5 +185,14 @@ class SubscribersAssignMultiProcessor extends modProcessor {
         }
         return $this->success();
     }
+    
+    /**
+     * {@inheritDoc}
+     *
+     * @return array
+     */
+    public function getLanguageTopics()
+    {
+        return ['user', 'goodnews:default'];
+    }
 }
-return 'SubscribersAssignMultiProcessor';
