950ec49161b498372c8f6ab8a3be0fe6e7afb1ed
Martin Gartner <martin.gartner@bitego.com>
2022-12-15 18:02:19 +0100

    Refactored ResourceMailingUpdate processor

--- core/components/goodnews/src/Processors/Mailing/ResourceMailingUpdate.php
+++ core/components/goodnews/src/Processors/Mailing/ResourceMailingUpdate.php
@@ -1,70 +1,70 @@
 <?php
-/**
- * GoodNews
- *
- * Copyright 2022 by bitego <office@bitego.com>
- *
- * GoodNews is free software; you can redistribute it and/or modify it under the
- * terms of the GNU General Public License as published by the Free Software
- * Foundation; either version 2 of the License, or (at your option) any later
- * version.
- *
- * GoodNews is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
- * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along with
- * this software; if not, write to the Free Software Foundation, Inc., 59 Temple
- * Place, Suite 330, Boston, MA 02111-1307 USA
- */
 
 /**
- * Overrides the modResourceUpdateProcessor to provide custom processor functionality
+ * This file is part of the GoodNews package.
+ *
+ * @copyright bitego (Martin Gartner)
+ * @license GNU General Public License v2.0 (and later)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Bitego\GoodNews\Processors\Mailing;
+
+use MODX\Revolution\modX;
+use MODX\Revolution\Processors\Resource\Update;
+use Bitego\GoodNews\Model\GoodNewsResourceContainer;
+use Bitego\GoodNews\Model\GoodNewsResourceMailing;
+use Bitego\GoodNews\Model\GoodNewsMailingMeta;
+use Bitego\GoodNews\RecipientsHandler;
+
+/**
+ * Overrides the MODX\Revolution\Processors\Resource\Update processor
+ * to provide custom processor functionality.
  *
  * @package goodnews
  */
-class GoodNewsResourceMailingUpdateProcessor extends modResourceUpdateProcessor {
-
-    public $classKey = 'GoodNewsResourceMailing';
-    public $languageTopics = array('resource','goodnews:default');
+class ResourceMailingUpdate extends Update
+{
+    public $classKey = GoodNewsResourceMailing::class;
+    public $languageTopics = ['resource','goodnews:default'];
 
     /** @var GoodNewsResourceMailing $object */
     public $object;
 
-    /** @var GoodNewsMailingMeta $object */
+    /** @var GoodNewsMailingMeta $meta */
     public $meta;
 
-     /** @var GoodnewsRecipientHandler $object */
-    public $goodnewsrecipienthandler;
-    
+    /** @var RecipientsHandler $recipientshandler */
+    public $recipientshandler;
+
     /** @var boolean $isPublishing */
     public $isPublishing = false;
 
     /**
-     * Create the GoodNewsResourceMailing (modResource) object for manipulation
+     * Create the GoodNewsResourceMailing (modResource) object for manipulation.
      *
      * {@inheritDoc}
      *
      * @return string|modResource
      */
-    public function initialize() {
+    public function initialize()
+    {
         $initialized = parent::initialize();
 
-        $corePath = $this->modx->getOption('goodnews.core_path', null, $this->modx->getOption('core_path').'components/goodnews/');
-        
-        $this->meta = $this->modx->getObject('GoodNewsMailingMeta', array('mailing_id'=>$this->object->get('id')));
+        $this->meta = $this->modx->getObject(
+            GoodNewsMailingMeta::class,
+            ['mailing_id' => $this->object->get('id')]
+        );
         if (!is_object($this->meta)) {
-            $this->meta = $this->modx->newObject('GoodNewsMailingMeta');
+            $this->meta = $this->modx->newObject(GoodNewsMailingMeta::class);
             if (!is_object($this->meta)) {
                 return $this->modx->lexicon('resource_err_update');
             }
         }
 
-        if (!$this->modx->loadClass('GoodNewsRecipientHandler', $corePath.'model/goodnews/', true, true)) {
-            $this->modx->log(modX::LOG_LEVEL_ERROR,'[GoodNews] Could not load GoodNewsRecipientHandler class.');
-            return $this->modx->lexicon('resource_err_update');
-        }
-        $this->goodnewsrecipienthandler = new GoodNewsRecipientHandler($this->modx);
+        $this->recipientshandler = new RecipientsHandler($this->modx);
 
         return $initialized;
     }
@@ -74,20 +74,21 @@ class GoodNewsResourceMailingUpdateProcessor extends modResourceUpdateProcessor 
      *
      * @return boolean|string
      */
-    public function beforeSet() {
+    public function beforeSet()
+    {
         $this->setProperty('clearCache', true);
         return parent::beforeSet();
     }
 
     /**
-     * Override modResourceUpdateProcessor::beforeSave
-     * 
+     * Override Update::beforeSave
+     *
      * {@inheritDoc}
      *
      * @return boolean
      */
-    public function beforeSave() {
-
+    public function beforeSave()
+    {
         // If sending has already been startet, the resource can't be changed any longer
         // (normaly this shouldn't happen as we don't provide an 'Edit' menu in this case)
         if ($this->meta->get('recipients_sent') != 0) {
@@ -100,43 +101,44 @@ class GoodNewsResourceMailingUpdateProcessor extends modResourceUpdateProcessor 
         $collection1 = array_filter(explode(',', $this->getProperty('collection1')));
         $collection2 = array_filter(explode(',', $this->getProperty('collection2')));
         $collection3 = array_filter(explode(',', $this->getProperty('collection3')));
-        $collections = array();
+        $collections = [];
         $collections['collection1'] = $collection1;
         $collections['collection2'] = $collection2;
         $collections['collection3'] = $collection3;
-        
+
         $this->meta->set('groups', $groups);
         $this->meta->set('categories', $categories);
         $this->meta->set('collections', serialize($collections));
 
-        $this->goodnewsrecipienthandler->collect(unserialize($groups), unserialize($categories));
-        $this->meta->set('recipients_total', $this->goodnewsrecipienthandler->getRecipientsTotal());
+        $this->recipientshandler->collect(unserialize($groups), unserialize($categories));
+        $this->meta->set('recipients_total', $this->recipientshandler->getRecipientsTotal());
         $this->object->addOne($this->meta);
-        
+
         // Copy container properties to mailing object properties
-        $container = $this->modx->getObject('GoodNewsResourceContainer', $this->object->get('parent'));
+        $container = $this->modx->getObject(GoodNewsResourceContainer::class, $this->object->get('parent'));
         if (is_object($container)) {
             $this->object->setProperties($container->getProperties('goodnews'), 'goodnews');
         }
-        
+
         $this->isPublishing = $this->object->isDirty('published') && $this->object->get('published');
 
         return parent::beforeSave();
     }
 
     /**
-     * Override modResourceUpdateProcessor::afterSave
+     * Override Update::afterSave
      *
      * {@inheritDoc}
      *
      * @return boolean
      */
-    public function afterSave() {
+    public function afterSave()
+    {
         $this->clearContainerCache();
 
         // update recipients list
-        $this->goodnewsrecipienthandler->updateRecipientsCollection($this->object->get('id'));
-        
+        $this->recipientshandler->updateRecipientsCollection($this->object->get('id'));
+
         return parent::afterSave();
     }
 
@@ -145,12 +147,13 @@ class GoodNewsResourceMailingUpdateProcessor extends modResourceUpdateProcessor 
      *
      * @return void
      */
-    public function clearContainerCache() {
+    public function clearContainerCache()
+    {
         $this->modx->cacheManager->refresh(array(
-            'db' => array(),
-            'auto_publish' => array('contexts' => array($this->object->get('context_key'))),
-            'context_settings' => array('contexts' => array($this->object->get('context_key'))),
-            'resource' => array('contexts' => array($this->object->get('context_key'))),
+            'db' => [],
+            'auto_publish' => ['contexts' => [$this->object->get('context_key')]],
+            'context_settings' => ['contexts' => [$this->object->get('context_key')]],
+            'resource' => ['contexts' => [$this->object->get('context_key')]],
         ));
     }
 
@@ -159,23 +162,45 @@ class GoodNewsResourceMailingUpdateProcessor extends modResourceUpdateProcessor 
      *
      * @return array|string
      */
-    public function cleanup() {
+    public function cleanup()
+    {
         $this->object->removeLock();
         $this->clearCache();
 
-        $returnArray = $this->object->get(array_diff(array_keys($this->object->_fields), array('content','ta','introtext','description','link_attributes','pagetitle','longtitle','menutitle','goodnews_container_settings','properties')));
+        $returnArray = $this->object->get(
+            array_diff(
+                array_keys($this->object->_fields),
+                [
+                    'content',
+                    'ta',
+                    'introtext',
+                    'description',
+                    'link_attributes',
+                    'pagetitle',
+                    'longtitle',
+                    'menutitle',
+                    'goodnews_container_settings',
+                    'properties'
+                ]
+            )
+        );
         foreach ($returnArray as $k => $v) {
-            if (strpos($k,'tv') === 0) {
+            if (strpos($k, 'tv') === 0) {
                 unset($returnArray[$k]);
             }
-            if (strpos($k,'setting_') === 0) {
+            if (strpos($k, 'setting_') === 0) {
                 unset($returnArray[$k]);
             }
         }
         $returnArray['class_key'] = $this->object->get('class_key');
         $this->workingContext->prepare(true);
-        $returnArray['preview_url'] = $this->modx->makeUrl($this->object->get('id'), $this->object->get('context_key'), '', 'full');
-        return $this->success('',$returnArray);
+        $returnArray['preview_url'] = $this->modx->makeUrl(
+            $this->object->get('id'),
+            $this->object->get('context_key'),
+            '',
+            'full'
+        );
+        return $this->success('', $returnArray);
     }
 
     /**
@@ -188,13 +213,13 @@ class GoodNewsResourceMailingUpdateProcessor extends modResourceUpdateProcessor 
      *
      * @return void
      */
-    public function prepareGroupsCategories() {
-    
+    public function prepareGroupsCategories()
+    {
         $nodes = explode(',', $this->getProperty('groupscategories'));
-        
-        $groups = array();
-        $categories = array();
-      
+
+        $groups = [];
+        $categories = [];
+
         foreach ($nodes as $node) {
             $nodeparts = explode('_', $node);
             if ($nodeparts[1] == 'gongrp') {
