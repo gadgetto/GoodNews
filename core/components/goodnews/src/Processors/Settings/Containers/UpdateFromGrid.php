cd240455a64c6269249feada6e9f93917479ef53
Martin Gartner <martin.gartner@bitego.com>
2022-12-18 12:00:22 +0100

    Refactored GoodNews resource container update from grid processor

---
+++ core/components/goodnews/src/Processors/Settings/Containers/UpdateFromGrid.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * This file is part of the GoodNews package.
+ *
+ * @copyright bitego (Martin Gartner)
+ * @license GNU General Public License v2.0 (and later)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Bitego\GoodNews\Processors\Settings\Containers;
+
+use Bitego\GoodNews\Processors\Settings\Containers\Update;
+
+/**
+ * GoodNews container update from grid processor (for settings panel)
+ *
+ * @package goodnews
+ * @subpackage processors
+ */
+class UpdateFromGrid extends Update
+{
+    public function initialize()
+    {
+        $data = $this->getProperty('data');
+        if (empty($data)) {
+            return $this->modx->lexicon('invalid_data');
+        }
+        $data = $this->modx->fromJSON($data);
+        if (empty($data)) {
+            return $this->modx->lexicon('invalid_data');
+        }
+        $this->setProperties($data);
+        $this->unsetProperty('data');
+        return parent::initialize();
+    }
+}
