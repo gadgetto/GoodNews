8c6cf171e3247f90e1d4c77bb67a8301e54162c5
Martin Gartner <martin.gartner@bitego.com>
2023-01-03 23:24:18 +0100

    New translations resource.inc.php (Polish)

---
+++ core/components/goodnews/lexicon/pl/resource.inc.php
@@ -0,0 +1,75 @@
+<?php
+
+/**
+ * GoodNews resource
+ *
+ * @package goodnews
+ * @subpackage lexicon
+ */
+
+$_lang['goodnews.container']                               = '';
+$_lang['goodnews.container_create_here']                   = '';
+$_lang['goodnews.container_new']                           = '';
+
+$_lang['goodnews.container_tab_container']                 = '';
+$_lang['goodnews.container_tab_settings_container']        = '';
+$_lang['goodnews.container_tab_settings_mailings']         = '';
+
+$_lang['goodnews.container_properties']                    = '';
+
+$_lang['goodnews.container_templates_category']            = '';
+$_lang['goodnews.container_templates_category_desc']       = '';
+$_lang['goodnews.container_mailing_template']              = '';
+$_lang['goodnews.container_mailing_template_desc']         = '';
+$_lang['goodnews.container_unsubscribe_resource']          = '';
+$_lang['goodnews.container_unsubscribe_resource_desc']     = '';
+$_lang['goodnews.container_profile_resource']              = '';
+$_lang['goodnews.container_profile_resource_desc']         = '';
+
+$_lang['goodnews.container_manage']                        = '';
+$_lang['goodnews.mailing_create_new']                      = '';
+$_lang['goodnews.container_unpublish']                     = '';
+$_lang['goodnews.container_publish']                       = '';
+$_lang['goodnews.container_undelete']                      = '';
+$_lang['goodnews.container_delete']                        = '';
+$_lang['goodnews.container_view']                          = '';
+$_lang['goodnews.manage_mailings']                         = '';
+
+$_lang['goodnews.mailing']                                 = '';
+$_lang['goodnews.mailing_readonly']                        = '';
+$_lang['goodnews.mailing_new']                             = '';
+$_lang['goodnews.mail_subject']                            = '';
+$_lang['goodnews.mail_subject_desc']                       = '';
+$_lang['goodnews.mail_summary']                            = '';
+$_lang['goodnews.mail_summary_desc']                       = '';
+$_lang['goodnews.mail_body']                               = '';
+$_lang['goodnews.mail_publishing']                         = '';
+$_lang['goodnews.mail_status']                             = '';
+$_lang['goodnews.mail_options']                            = '';
+$_lang['goodnews.mail_sending_scheduled']                  = '';
+$_lang['goodnews.mail_sending_scheduled_desc']             = '';
+$_lang['goodnews.mail_send_to']                            = '';
+$_lang['goodnews.mail_groups_categories']                  = '';
+$_lang['goodnews.mail_template']                           = '';
+$_lang['goodnews.mail_template_desc']                      = '';
+$_lang['goodnews.mail_format']                             = '';
+$_lang['goodnews.mail_format_desc']                        = '';
+$_lang['goodnews.mail_format_html']                        = '';
+$_lang['goodnews.mail_format_plaintxt']                    = '';
+
+$_lang['goodnews.mailing_resource_collection']             = '';
+$_lang['goodnews.mailing_resource_collection_desc']        = '';
+$_lang['goodnews.mailing_rc_container']                    = '';
+$_lang['goodnews.mailing_rc_resource']                     = '';
+$_lang['goodnews.mailing_rc_resources']                    = '';
+$_lang['goodnews.mailing_rc_resources_none']               = '';
+$_lang['goodnews.mailing_rc_parent_filter']                = '';
+$_lang['goodnews.mailing_rc_resources_all']                = '';
+$_lang['goodnews.mailing_rc_search_filter']                = '';
+$_lang['goodnews.mailing_rc_filter_clear']                 = '';
+
+$_lang['setting_goodnews.default_container_template']      = '';
+$_lang['setting_goodnews.default_container_template_desc'] = '';
+
+$_lang['setting_goodnews.default_mailing_template']        = '';
+$_lang['setting_goodnews.default_mailing_template_desc']   = '';
