9f17c94879cdb428d3d3e53cfc5f93f01042c48d
Martin Gartner <martin.gartner@bitego.com>
2023-01-03 23:24:09 +0100

    New translations frontend.inc.php (Turkish)

---
+++ core/components/goodnews/lexicon/tr/frontend.inc.php
@@ -0,0 +1,70 @@
+<?php
+
+/**
+ * GoodNews frontend
+ *
+ * @package goodnews
+ * @subpackage lexicon
+ */
+
+$_lang['goodnews.user_err_save']                    = '';
+$_lang['goodnews.user_err_nf_email']                = '';
+$_lang['goodnews.profile_err_nf']                   = '';
+$_lang['goodnews.profile_err_save']                 = '';
+$_lang['goodnews.profile_err_unsubscribe']          = '';
+$_lang['goodnews.profile_updated']                  = '';
+$_lang['goodnews.profile_unsubscription']           = '';
+
+$_lang['goodnews.email_no_recipient']               = '';
+$_lang['goodnews.email_not_sent']                   = '';
+
+$_lang['goodnews.validator_form_error']             = '';
+$_lang['goodnews.validator_field_required']         = '';
+$_lang['goodnews.validator_field_not_empty']        = '';
+$_lang['goodnews.validator_email_multiple']         = '';
+$_lang['goodnews.validator_email_taken']            = '';
+$_lang['goodnews.validator_email_invalid']          = '';
+$_lang['goodnews.validator_email_invalid_domain']   = '';
+$_lang['goodnews.validator_username_taken']         = '';
+$_lang['goodnews.validator_password_dont_match']    = '';
+$_lang['goodnews.validator_password_not_confirmed'] = '';
+$_lang['goodnews.validator_min_length']             = '';
+$_lang['goodnews.validator_max_length']             = '';
+$_lang['goodnews.validator_min_value']              = '';
+$_lang['goodnews.validator_max_value']              = '';
+$_lang['goodnews.validator_contains']               = '';
+$_lang['goodnews.validator_not_number']             = '';
+$_lang['goodnews.validator_range_invalid']          = '';
+$_lang['goodnews.validator_range']                  = '';
+$_lang['goodnews.validator_not_date']               = '';
+$_lang['goodnews.validator_not_lowercase']          = '';
+$_lang['goodnews.validator_not_uppercase']          = '';
+$_lang['goodnews.validator_not_regexp']             = '';
+
+$_lang['goodnews.spam_blocked']                     = '';
+$_lang['goodnews.spam_marked']                      = '';
+
+$_lang['goodnews.activation_email_subject']         = '';
+$_lang['goodnews.subscription_email_subject']       = '';
+$_lang['goodnews.resubscription_email_subject']     = '';
+$_lang['goodnews.requestlinks_email_subject']       = '';
+
+$_lang['goodnews.requestlinks_success']             = '';
+
+$_lang['goodnews.email']                            = '';
+$_lang['goodnews.username']                         = '';
+$_lang['goodnews.password']                         = '';
+$_lang['goodnews.password_confirm']                 = '';
+$_lang['goodnews.fullname']                         = '';
+$_lang['goodnews.address']                          = '';
+$_lang['goodnews.city']                             = '';
+$_lang['goodnews.zip']                              = '';
+$_lang['goodnews.state']                            = '';
+$_lang['goodnews.country']                          = '';
+$_lang['goodnews.mobilephone']                      = '';
+$_lang['goodnews.phone']                            = '';
+$_lang['goodnews.fax']                              = '';
+$_lang['goodnews.website']                          = '';
+$_lang['goodnews.update_profile']                   = '';
+$_lang['goodnews.subscribe']                        = '';
+$_lang['goodnews.register']                         = '';
