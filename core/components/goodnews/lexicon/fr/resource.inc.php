71dd43cddb5ad580ce84fee87742f72f179aba44
Martin Gartner <martin.gartner@bitego.com>
2023-01-03 21:01:04 +0100

    New translations resource.inc.php (French)

--- core/components/goodnews/lexicon/fr/resource.inc.php
+++ core/components/goodnews/lexicon/fr/resource.inc.php
@@ -23,7 +23,7 @@
  *
  * @package goodnews
  * @subpackage lexicon
- * @language fr
+ * @language en
  */
 
 $_lang['goodnews.container']                               = 'GoodNews';
@@ -55,6 +55,7 @@ $_lang['goodnews.container_view']                          = 'Voir le Conteneur 
 $_lang['goodnews.manage_mailings']                         = 'Gestion des Mailings';
 
 $_lang['goodnews.mailing']                                 = 'Mailing';
+$_lang['goodnews.mailing_readonly']                        = 'Read Only';
 $_lang['goodnews.mailing_new']                             = 'Nouveau Mailing';
 $_lang['goodnews.mail_subject']                            = 'Sujet du Mail';
 $_lang['goodnews.mail_subject_desc']                       = 'Le sujet du mail.';
