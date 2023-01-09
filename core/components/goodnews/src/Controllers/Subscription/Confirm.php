339e075b3eb5a601881388e69f53b4ce12d3ec4f
Martin Gartner <martin.gartner@bitego.com>
2023-01-09 12:02:16 +0100

    Refactored Confirm controller

--- core/components/goodnews/src/Controllers/Subscription/Confirm.php
+++ core/components/goodnews/src/Controllers/Subscription/Confirm.php
@@ -1,57 +1,56 @@
 <?php
-/**
- * GoodNews
- *
- * Copyright 2012 by bitego <office@bitego.com>
- * Based on code from Login add-on
- * Copyright 2010 by Shaun McCormick <shaun@modx.com>
- * Modified by bitego - 10/2013
- *
- * GoodNews is free software; you can redistribute it and/or modify it under the
- * terms of the GNU General Public License as published by the Free Software
- * Foundation; either version 2 of the License, or (at your option) any later
- * version.
- *
- * GoodNews is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
- * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along with
- * this software; if not, write to the Free Software Foundation, Inc., 59 Temple
- * Place, Suite 330, Boston, MA 02111-1307 USA
- */
 
 /**
- * Class which confirms a user's subscription after activation
+ * This file is part of the GoodNews package.
+ *
+ * @copyright bitego (Martin Gartner)
+ * @license GNU General Public License v2.0 (and later)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Bitego\GoodNews\Controllers\Subscription;
+
+use MODX\Revolution\modUser;
+use MODX\Revolution\modUserProfile;
+use MODX\Revolution\Registry\modRegistry;
+use MODX\Revolution\Registry\modFileRegister;
+use Bitego\GoodNews\Model\GoodNewsSubscriberMeta;
+
+/**
+ * Controller class which confirms a user's subscription after activation.
  *
  * @package goodnews
  * @subpackage controllers
  */
 
-class GoodNewsSubscriptionConfirmController extends GoodNewsSubscriptionController {
+class Confirm extends Base
+{
     /** @var modUser $user */
-    public $user;
-    
+    public $user = null;
+
     /** @var modUserProfile $profile */
-    public $profile;
-    
+    public $profile = null;
+
     /** @var GoodNewsSubscriberMeta $subscribermeta */
-    public $subscribermeta;
+    public $subscribermeta = null;
 
     /** @var string $username */
-    public $username;
-    
+    public $username = '';
+
     /** @var string $password */
-    public $password;
-    
+    public $password = '';
+
     /**
      * initialize function.
-     * 
+     *
      * @access public
      * @return void
      */
-    public function initialize() {
-        $this->setDefaultProperties(array(
+    public function initialize()
+    {
+        $this->setDefaultProperties([
             'sendSubscriptionEmail'    => true,
             'unsubscribeResourceId'    => '',
             'profileResourceId'        => '',
@@ -60,39 +59,42 @@ class GoodNewsSubscriptionConfirmController extends GoodNewsSubscriptionControll
             'subscriptionEmailTplAlt'  => '',
             'subscriptionEmailTplType' => 'modChunk',
             'errorPage'                => false,
-        ));
+        ]);
     }
 
     /**
      * process function.
-     * 
+     *
      * @access public
      * @return void
      */
-    public function process() {
+    public function process()
+    {
         $this->verifyManifest();
         $this->getSubscriber();
         $this->validatePassword();
-        
+
         $result = $this->runProcessor('ConfirmSubscription');
-        
+
         return '';
     }
 
     /**
-     * Verify that the username/password hashes were correctly sent (base64 encoded in URL) to prevent middle-man attacks.
+     * Verify that the username/password hashes were correctly sent
+     * (base64 encoded in URL) to prevent middle-man attacks.
      *
      * @access public
      * @return boolean
      */
-    public function verifyManifest() {
+    public function verifyManifest()
+    {
         $verified = false;
         if (empty($_REQUEST['lp']) || empty($_REQUEST['lu'])) {
             $this->redirectAfterFailure();
         } else {
-            // get username and password from query params
-            $this->username = $this->goodnewssubscription->base64UrlDecode($_REQUEST['lu']);
-            $this->password = $this->goodnewssubscription->base64UrlDecode($_REQUEST['lp']);
+            // Get username and password from query params
+            $this->username = $this->subscription->base64UrlDecode($_REQUEST['lu']);
+            $this->password = $this->subscription->base64UrlDecode($_REQUEST['lp']);
             $verified = true;
         }
         return $verified;
@@ -104,19 +106,20 @@ class GoodNewsSubscriptionConfirmController extends GoodNewsSubscriptionControll
      * @access public
      * @return void
      */
-    public function getSubscriber() {
-        $this->user = $this->modx->getObject('modUser', array('username' => $this->username));
+    public function getSubscriber()
+    {
+        $this->user = $this->modx->getObject(modUser::class, ['username' => $this->username]);
         if (!is_object($this->user) || $this->user->get('active')) {
             $this->redirectAfterFailure();
         }
         $userID = $this->user->get('id');
-        
-        $this->profile = $this->modx->getObject('modUserProfile', array('internalKey' => $userID));
+
+        $this->profile = $this->modx->getObject(modUserProfile::class, ['internalKey' => $userID]);
         if (!is_object($this->profile)) {
             $this->redirectAfterFailure();
         }
-        
-        $this->subscribermeta = $this->modx->getObject('GoodNewsSubscriberMeta', array('subscriber_id' => $userID));
+
+        $this->subscribermeta = $this->modx->getObject(GoodNewsSubscriberMeta::class, ['subscriber_id' => $userID]);
         if (!is_object($this->subscribermeta)) {
             $this->redirectAfterFailure();
         }
@@ -128,13 +131,23 @@ class GoodNewsSubscriptionConfirmController extends GoodNewsSubscriptionControll
      * @access public
      * @return boolean
      */
-    public function validatePassword() {
-        $this->modx->getService('registry', 'registry.modRegistry');
-        $this->modx->registry->addRegister('goodnewssubscription','registry.modFileRegister');
-        $this->modx->registry->goodnewssubscription->connect();
-        $this->modx->registry->goodnewssubscription->subscribe('/useractivation/'.$this->user->get('username'));
-        $msgs = $this->modx->registry->goodnewssubscription->read();
-        if (empty($msgs)) $this->modx->sendErrorPage();
+    public function validatePassword()
+    {
+        // Read new password from modRegistry to prevent middleman attacks.
+        if (!$this->modx->services->has('registry')) {
+            $modx = &$this->modx;
+            $this->modx->services->add('registry', function ($c) use ($modx) {
+                return new modRegistry($modx);
+            });
+        }
+        $registry = $this->modx->services->get('registry');
+        $registry->addRegister('goodnewssubscription', modFileRegister::class);
+        $registry->goodnewssubscription->connect();
+        $registry->goodnewssubscription->subscribe('/useractivation/' . $this->user->get('username'));
+        $msgs = $registry->goodnewssubscription->read();
+        if (empty($msgs)) {
+            $this->modx->sendErrorPage();
+        }
         $found = false;
         foreach ($msgs as $msg) {
             if ($msg == $this->password) {
@@ -147,4 +160,3 @@ class GoodNewsSubscriptionConfirmController extends GoodNewsSubscriptionControll
         return $found;
     }
 }
-return 'GoodNewsSubscriptionConfirmController';
