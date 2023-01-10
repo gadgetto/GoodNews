0c4a56504741a9ffe9d9dd4c34604d8f020308ae
Martin Gartner <martin.gartner@bitego.com>
2023-01-10 15:56:28 +0100

    Refactored UpdateProfile processor

--- core/components/goodnews/src/Processors/Subscription/UpdateProfile.php
+++ core/components/goodnews/src/Processors/Subscription/UpdateProfile.php
@@ -1,74 +1,89 @@
 <?php
-/**
- * GoodNews
- *
- * Copyright 2012 by bitego <office@bitego.com>
- * Based on code from Login add-on
- * Copyright 2012 by Jason Coward <jason@modx.com> and Shaun McCormick <shaun@modx.com>
- * Modified by bitego - 10/2013
- *
- * GoodNews is free software; you can redistribute it and/or modify it under the
- * terms of the GNU General Public License as published by the Free Software
- * Foundation; either version 2 of the License, or (at your option) any later
- * version.
- *
- * GoodNews is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
- * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along with
- * this software; if not, write to the Free Software Foundation, Inc., 59 Temple
- * Place, Suite 330, Boston, MA 02111-1307 USA
- */
 
 /**
- * Processor class which handles subscription update forms
+ * This file is part of the GoodNews package.
+ *
+ * @copyright bitego (Martin Gartner)
+ * @license GNU General Public License v2.0 (and later)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Bitego\GoodNews\Processors\Subscription;
+
+use Bitego\GoodNews\Model\GoodNewsGroupMember;
+use Bitego\GoodNews\Model\GoodNewsCategoryMember;
+use Bitego\GoodNews\Processors\Subscription\Base;
+
+/**
+ * Processor class which handles subscription update.
  *
  * @package goodnews
  * @subpackage processors
  */
 
-class GoodNewsSubscriptionUpdateProfileProcessor extends GoodNewsSubscriptionProcessor {
+class UpdateProfile extends Base
+{
     /** @var modUser $user */
-    public $user;
-    
+    public $user = null;
+
     /** @var modUserProfile $profile */
-    public $profile;
-    
+    public $profile = null;
+
     /** @var GoodNewsSubscriberMeta $subscribermeta */
-    public $subscribermeta;
-    
+    public $subscribermeta = null;
+
     /**
      * @return boolean|string
      */
-    public function process() {
-        $this->user           = $this->controller->user;
-        $this->profile        = $this->controller->profile;
+    public function process()
+    {
+        $this->user = $this->controller->user;
+        $this->profile = $this->controller->profile;
         $this->subscribermeta = $this->controller->subscribermeta;
-        
+
         $this->cleanseFields();
 
-        //$dic = $this->controller->dictionary->toArray();
+        //$dic = $this->controller->dictionary->to[];
         //$this->modx->log(modX::LOG_LEVEL_INFO, '[GoodNews] dictionary: '.$this->modx->toJson($dic));
 
         $this->setExtended();
-        
+
         // Save user changes
         $this->setUserFields();
         if (!$this->save()) {
-            $this->modx->log(modX::LOG_LEVEL_ERROR, '[GoodNews] Could not save changed subscriber user data - '.$this->user->get('id').' with username: '.$this->user->get('username'));
+            $this->modx->log(
+                modX::LOG_LEVEL_ERROR,
+                '[GoodNews] Could not save changed subscriber user data - ' .
+                $this->user->get('id') .
+                ' with username: ' .
+                $this->user->get('username')
+            );
             return $this->modx->lexicon('goodnews.profile_err_save');
         }
 
         // Update goodnews group member
         if (!$this->updateGroupMember()) {
-            $this->modx->log(modX::LOG_LEVEL_ERROR, '[GoodNews] Could not save changed subscriber group member data - '.$this->user->get('id').' with username: '.$this->user->get('username'));
+            $this->modx->log(
+                modX::LOG_LEVEL_ERROR,
+                '[GoodNews] Could not save changed subscriber group member data - ' .
+                $this->user->get('id') .
+                ' with username: ' .
+                $this->user->get('username')
+            );
             return $this->modx->lexicon('goodnews.user_err_save');
         }
 
         // Update goodnews category member
         if (!$this->updateCategoryMember()) {
-            $this->modx->log(modX::LOG_LEVEL_ERROR, '[GoodNews] Could not save changed subscriber category member data - '.$this->user->get('id').' with username: '.$this->user->get('username'));
+            $this->modx->log(
+                modX::LOG_LEVEL_ERROR,
+                '[GoodNews] Could not save changed subscriber category member data - ' .
+                $this->user->get('id') .
+                ' with username: ' .
+                $this->user->get('username')
+            );
             return $this->modx->lexicon('goodnews.user_err_save');
         }
 
@@ -82,12 +97,14 @@ class GoodNewsSubscriptionUpdateProfileProcessor extends GoodNewsSubscriptionPro
      *
      * @return void
      */
-    public function cleanseFields() {
+    public function cleanseFields()
+    {
         $submitVar = $this->controller->getProperty('submitVar', 'goodnews-updateprofile-btn');
-        
         $this->dictionary->remove('nospam');
         $this->dictionary->remove('blank');
-        if (!empty($submitVar)) { $this->dictionary->remove($submitVar); }
+        if (!empty($submitVar)) {
+            $this->dictionary->remove($submitVar);
+        }
     }
 
     /**
@@ -95,7 +112,8 @@ class GoodNewsSubscriptionUpdateProfileProcessor extends GoodNewsSubscriptionPro
      *
      * @return void
      */
-    public function setUserFields() {
+    public function setUserFields()
+    {
         $fields = $this->dictionary->toArray();
         foreach ($fields as $key => $value) {
             $this->profile->set($key, $value);
@@ -107,17 +125,18 @@ class GoodNewsSubscriptionUpdateProfileProcessor extends GoodNewsSubscriptionPro
      *
      * @return boolean
      */
-    public function updateGroupMember() {
+    public function updateGroupMember()
+    {
         $userid = $this->user->get('id');
-        
+
         // First remove all previously stored group member entries
-        $this->modx->removeCollection('GoodNewsGroupMember', array('member_id' => $userid));        
-        
+        $this->modx->removeCollection(GoodNewsGroupMember::class, ['member_id' => $userid]);
+
         $selectedGroups = $this->dictionary->get('gongroups');
-        
+
         $success = true;
         foreach ($selectedGroups as $grpid) {
-            $groupmember = $this->modx->newObject('GoodNewsGroupMember');
+            $groupmember = $this->modx->newObject(GoodNewsGroupMember::class);
             $groupmember->set('goodnewsgroup_id', $grpid);
             $groupmember->set('member_id', $userid);
             if (!$groupmember->save()) {
@@ -126,23 +145,24 @@ class GoodNewsSubscriptionUpdateProfileProcessor extends GoodNewsSubscriptionPro
         }
         return $success;
     }
-    
+
     /**
      * Set and update the category member data.
      *
      * @return boolean
      */
-    public function updateCategoryMember() {
+    public function updateCategoryMember()
+    {
         $userid = $this->user->get('id');
 
         // First remove all previously stored category member entries
-        $this->modx->removeCollection('GoodNewsCategoryMember', array('member_id' => $userid));
+        $this->modx->removeCollection(GoodNewsCategoryMember::class, ['member_id' => $userid]);
 
         $selectedCategories = $this->dictionary->get('goncategories');
 
         $success = true;
         foreach ($selectedCategories as $catid) {
-            $categorymember = $this->modx->newObject('GoodNewsCategoryMember');
+            $categorymember = $this->modx->newObject(GoodNewsCategoryMember::class);
             $categorymember->set('goodnewscategory_id', $catid);
             $categorymember->set('member_id', $userid);
             if (!$categorymember->save()) {
@@ -157,9 +177,10 @@ class GoodNewsSubscriptionUpdateProfileProcessor extends GoodNewsSubscriptionPro
      *
      * @return void
      */
-    public function setExtended() {
+    public function setExtended()
+    {
         $useExtended = $this->controller->getProperty('useExtended', false, 'isset');
-        
+
         if ($useExtended) {
             // first cut out regular fields
             $excludeExtended = $this->controller->getProperty('excludeExtended', '');
@@ -167,20 +188,26 @@ class GoodNewsSubscriptionUpdateProfileProcessor extends GoodNewsSubscriptionPro
 
             $profileFields = $this->profile->toArray();
             $userFields = $this->user->toArray();
-            
-            $newExtended = array();
+
+            $newExtended = [];
             $fields = $this->dictionary->toArray();
-            
+
             foreach ($fields as $field => $value) {
                 $isValidExtended = true;
-                if (isset($profileFields[$field]) || isset($userFields[$field]) || in_array($field, $excludeExtended) || $field == 'nospam' || $field == 'nospam:blank') {
+                if (
+                    isset($profileFields[$field]) ||
+                    isset($userFields[$field]) ||
+                    in_array($field, $excludeExtended) ||
+                    $field == 'nospam' ||
+                    $field == 'nospam:blank'
+                ) {
                     $isValidExtended = false;
                 }
                 if ($isValidExtended) {
                     $newExtended[$field] = $value;
                 }
             }
-            // now merge with existing extended data
+            // Now merge with existing extended data
             $extended = $this->profile->get('extended');
             $extended = is_array($extended) ? array_merge($extended, $newExtended) : $newExtended;
             $this->profile->set('extended', $extended);
@@ -192,9 +219,10 @@ class GoodNewsSubscriptionUpdateProfileProcessor extends GoodNewsSubscriptionPro
      *
      * @return boolean
      */
-    public function save() {
-        $this->controller->user->addOne($this->profile, 'Profile');
-        $saved = $this->controller->user->save();
+    public function save()
+    {
+        $this->user->addOne($this->profile, 'Profile');
+        $saved = $this->user->save();
         return $saved;
     }
 
@@ -203,29 +231,29 @@ class GoodNewsSubscriptionUpdateProfileProcessor extends GoodNewsSubscriptionPro
      *
      * @return void
      */
-    public function runPostHooks() {
+    public function runPostHooks()
+    {
         $postHooks = $this->controller->getProperty('postHooks', '');
         $this->controller->loadHooks('postHooks');
-        
-        $fields = $this->controller->dictionary->toArray();
-        //$fields = $this->dictionary->toArray(); ??? bug in original file?
-        $fields['goodnewssubscription.user'] = &$this->controller->user;
-        $fields['goodnewssubscription.profile'] =&$this->profile;
-        
+
+        $fields = $this->dictionary->toArray();
+        $fields['subscription.user'] = &$this->user;
+        $fields['subscription.profile'] = &$this->profile;
+
         $this->controller->postHooks->loadMultiple($postHooks, $fields);
 
-        /* process hooks */
+        // Process hooks
         if ($this->controller->postHooks->hasErrors()) {
-            $errors = array();
+            $errors = [];
             $errTpl = $this->controller->getProperty('errTpl');
             $errs = $this->controller->postHooks->getErrors();
             foreach ($errs as $key => $error) {
                 $errors[$key] = str_replace('[[+error]]', $error, $errTpl);
             }
             $placeholderPrefix = $this->controller->getProperty('placeholderPrefix', '');
-            $this->modx->toPlaceholders($errors, $placeholderPrefix.'error');
+            $this->modx->toPlaceholders($errors, $placeholderPrefix . 'error');
             $errorMsg = $this->controller->postHooks->getErrorMessage();
-            $this->modx->toPlaceholder('message', $errorMsg, $placeholderPrefix.'error');
+            $this->modx->toPlaceholder('message', $errorMsg, $placeholderPrefix . 'error');
         }
     }
 
@@ -234,10 +262,10 @@ class GoodNewsSubscriptionUpdateProfileProcessor extends GoodNewsSubscriptionPro
      *
      * @return void
      */
-    public function handleSuccess() {
+    public function handleSuccess()
+    {
         $placeholderPrefix = $this->controller->getProperty('placeholderPrefix', '');
-        $successMsg        = $this->controller->getProperty('successMsg', $this->modx->lexicon('goodnews.profile_updated'));
-        $this->modx->toPlaceholder('message', $successMsg, $placeholderPrefix.'success');
+        $successMsg = $this->controller->getProperty('successMsg', $this->modx->lexicon('goodnews.profile_updated'));
+        $this->modx->toPlaceholder('message', $successMsg, $placeholderPrefix . 'success');
     }
 }
-return 'GoodNewsSubscriptionUpdateProfileProcessor';
